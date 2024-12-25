1b556d58287c6cc8a4fadc5018daa0f6008e0a5b
Dinesh Reddy <dinu350930@gmail.com>
2024-12-25 08:03:01 +0530

    25-12-2024

---
+++ pp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Header from './components/Header'
+function App() {
+  
+  return (
+    <>
+    D
+       <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ pp/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { logo } from '../assets'
+const Header = () => {
+  return (
+    <Header>
+        <div>
+            <img src={logo} alt="Dinesh Reddy" />
+        </div>
+
+    </Header>
+  )
+}
+
+export default Header
